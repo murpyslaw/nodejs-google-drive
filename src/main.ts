1d0bc23612d4855b8c2030b665a0c8d183ead480
Radoslav Ivanov <rivanov2@protonmail.com>
2018-09-29 19:02:59 -0500

    :heavy_check_mark: get approval code from OAuth

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
+// clipshare101:T&cVI0f$14K7WQ*n$n07
+import { app, BrowserWindow } from 'electron';
+import * as fs from 'fs';
+import { OAuth2Client } from 'google-auth-library';
+import { google } from 'googleapis';
+import * as path from 'path';
+import * as readline from 'readline';
+import { URL } from 'url';
+
+// If modifying these scopes, delete token.json.
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+const TOKEN_PATH = 'token.json';
 
 let mainWindow: Electron.BrowserWindow;
 
@@ -7,17 +19,36 @@ function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     height: 600,
-    width: 800,
+    width: 800
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadFile(path.join(__dirname, '../index.html'));
+
+  // Load client secrets from a local file.
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Drive API.
+    authorize(JSON.parse(content.toString()), null);
+    // https://accounts.google.com/o/oauth2/approval/v2/approvalnativeapp?auto=false
+    //&response=code%3D4%2FagCHB-6DkTvVFf80ts-bkvfItwrvLiMYMmeRLu9jEAxzwFlzaX0eurw&hl=en&
+    //approvalCode=4/FagCHB-6DkTvVFf80ts-bkvfItwrvLiMYMmeRLu9jEAxzwFlzaX0eurw
+  });
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
+  mainWindow.webContents.on('will-navigate', (ev: any, url: string) => {
+    if (url.includes('approval')) {
+      // Get the approvalCode from URL params
+      const parsed = new URL(url);
+      console.log(parsed.searchParams.get('approvalCode'));
+      // Main Logic Here
+    }
+  });
+
   // Emitted when the window is closed.
-  mainWindow.on("closed", () => {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -28,18 +59,18 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   // On OS X it"s common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
@@ -49,3 +80,59 @@ app.on("activate", () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials: any, callback?: Function) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token.toString()));
+    if (callback) callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client: OAuth2Client, callback: Function) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  mainWindow.loadURL(authUrl);
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', code => {
+    rl.close();
+    oAuth2Client.getToken(code, (err: any, token: any) => {
+      if (err) return console.error('Error retrieving access token', err);
+      if (token) {
+        oAuth2Client.setCredentials(token);
+      }
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
+        if (err) console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
